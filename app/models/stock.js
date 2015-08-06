41e71e3e6c2036ad08c8d866ae212535929b19e0
RONALD LUMP <ronaldlump@RONALDs-MacBook-Air.local>
2015-08-06 08:50:09 -0500

    rework route

--- app/models/stock.js
+++ app/models/stock.js
@@ -1,6 +1,6 @@
 import DS from "ember-data";
 
 export default DS.Model.extend({
-  name: DS.attr('string'),
-  symbol: DS.attr('string')
+  symbol: DS.attr('string'),
+  name: DS.attr('string')
 });
